915a476d781913eed687a66a3093a2cd57dbd1ea
mtso <mtso@tuta.io>
2017-03-18 11:55:16 -0700

    Add bearer strategy to auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
 const User = require('../models/user');
 const Client = require('../models/client');
+const Token = require('../models/token');
 
 passport.use(new BasicStrategy(
   function(username, password, callback) {
@@ -49,6 +52,36 @@ passport.use(
   })
 );
 
+passport.use(new BearerStrategy(
+  function(accessToken, callback) {
+    Token.findOne({value: accessToken},
+      function(err, token) {
+        if (err) {
+          return callback(err);
+
+        } else if (!token) {
+          return callback(null, false);
+        }
+
+        User.findOne({_id: token.userId},
+          function(err, result) {
+            if (err) {
+              return callback(err);
+
+            } else if (!user) {
+              return callback(null, false);
+            }
+
+            // No scope. (I guess that means access to everything?)
+            callback(null, user, {scope: '*'});
+          }
+        );
+      }
+    );
+  })
+);
+
 // Set session to true to store session variables between API calls.
 exports.isAuthenticated = passport.authenticate('basic', {session: false});
 exports.isClientAuthenticated = passport.authenticate('client-basic', {session: false});
+exports.isBearerAuthenticated = passport.authenticate('bearer', {session: false});
